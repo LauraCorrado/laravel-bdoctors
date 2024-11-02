8b6b41604fc260d3cb1246d2003df007c43c578b
Corfaya <lauracorrado0696@gmail.com>
2024-11-02 10:56:14 +0100

    [UPD] new logo imported in header and in html head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'BDoctors') }}</title>
+    <link rel="icon" href="{{ Vite::asset('/resources/img/logo.png') }}" type="image/x-icon">
 
 
     <!-- Fonts -->
