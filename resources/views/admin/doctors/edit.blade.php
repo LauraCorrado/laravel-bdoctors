20203b7482932cc9481fd91201922f6c6d17b624
pierobramanti <piero.bramanti94@gmail.com>
2024-11-02 20:46:03 +0100

    [UPD] copied structure of create in edit

---
+++ resources/views/admin/doctors/edit.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-12">
+            <div class="mb-5 text-center">
+                <h1>Grazie per la tua iscrizione!</h1>
+                <h2 class="m-2">Crea qui il tuo profilo</h2>
+                <p>I campi contrassegnati con <strong>*</strong> sono obbligatori</p>
+            </div>
+                <form action="{{ route('admin.doctors.store') }}" method="POST" class="my-4 text-center">
+                    @csrf
+                    <div class="row g-3">
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <label for="user_name" class="form-label fw-bolder m-0 py-1">Nome*</label>
+                            <input type="text" name="user_name" id="user_name" placeholder="Nome" class="form-control" required>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <label for="user_surname" class="form-label fw-bolder m-0 py-1">Cognome*</label>
+                            <input type="text" name="user_surname" id="user_surname" placeholder="Cognome" class="form-control" required>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <label for="address" class="form-label fw-bolder m-0 py-1">Indirizzo*</label>
+                            <input type="text" name="address" id="address" placeholder="Indirizzo" class="form-control" required>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <label for="city" class="form-label fw-bolder m-0 py-1">Città*</label>
+                            <input type="text" name="city" id="city" placeholder="Città" class="form-control" required>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <label for="phone_number" class="form-label fw-bolder m-0 py-1">Numero di telefono*</label>
+                            <input type="text" name="phone_number" id="phone_number" placeholder="Numero di telefono" class="form-control" required>
+                        </div>
+
+                        <div class="col-12">
+                            <label class="form-label fw-bolder m-0 py-1" for="fields">Specializzazioni*</label>
+                            <p>Seleziona uno o più campi di specializzazione</p>
+                                <div class="row py-3">
+                                    @foreach($fields as $field)
+                                    <div class="col-6 col-md-4 d-flex">
+                                        <div class="form-check-inline m-0">
+                                            <input type="checkbox" name="fields[]" class="form-check-input" value="{{$field->id}}" {{ is_array(old('fields')) && in_array($field->id, old('fields')) ? 'checked' : ''}}>
+                                            <label class="form-check-label ps-1">{{$field->name}}</label>
+                                        </div>
+
+                                    </div>
+                                    @endforeach
+                                </div>
+                        </div>
+
+                        <div class="col-12">
+                            <label for="performance" class="form-label fw-bolder m-0 py-1">Prestazioni*</label>
+                            <textarea name="performance" id="performance" rows="4" class="form-control" placeholder="Descrivi le tue prestazioni" required></textarea>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="mt-3 save">Salva i dati</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
