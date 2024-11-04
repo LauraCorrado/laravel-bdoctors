298ea71a89a82fb70cd3bcb24b5412fbc0633615
Corfaya <lauracorrado0696@gmail.com>
2024-11-04 20:06:42 +0100

    [UPD] dashboard first update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
+    <h1 class="fs-4 text-secondary my-4">
+        {{ __('Dashboard di ') }} {{$doctor->user_name}} {{$doctor->user_surname}}
+    </h1>
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }} Benvenuto {{$doctor->user_name}}</div>
-
+                <div class="card-header"><h5 class="m-0">Benvenuto/a, {{$doctor->user_name}}</h5></div>
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    <p>{{ __('Hai effettuato con successo il login!') }}</p>
+                    <p class="m-0">{{ __('Questa è la tua dashboard.') }}</p>
                 </div>
             </div>
         </div>
