7382fd3482d26c9e7296fc4bbdb64cbc045384bf
Corfaya <lauracorrado0696@gmail.com>
2024-11-04 19:59:11 +0100

    [UPD] requests update

--- app/Http/Requests/StoreDoctorRequest.php
+++ app/Http/Requests/StoreDoctorRequest.php
@@ -41,10 +41,21 @@ class StoreDoctorRequest extends FormRequest
     public function messages()
     {
         return [
+            'user_name.required' => 'Questo campo è obbligatorio.',
             'user_name.min' => 'Il nome deve contenere almeno 4 caratteri.',
+            'user_name.max' => 'Il nome può contenere massimo :max caratteri.',
+            'user_surname.required' => 'Questo campo è obbligatorio.',
             'user_surname.min' => 'Il cognome deve contenere almeno 4 caratteri.',
+            'user_surname.max' => 'Il cognome può contenere massimo :max caratteri.',
+            'city.required' => 'Questo campo è obbligatorio.',
+            'city.max' => 'La città non può superare i :max caratteri.',
+            'address.required' => 'Questo campo è obbligatorio.',
             'address.min' => 'L\'indirizzo deve contenere almeno 7 caratteri.',
+            'address.max' => 'L\'indirizzo non può superare i :max caratteri.',
+            'phone_number.required' => 'Questo campo è obbligatorio.',
             'phone_number.min' => 'Il numero di telefono deve contenere almeno 10 cifre.',
+            'phone_number.max' => 'Il numero di telefono non può essere superiore a :max cifre.',
+            'performance.required' => 'Questo campo è obbligatorio.',
             'performance.min' => 'La descrizione delle prestazioni deve contenere almeno 30 caratteri.',
             'performance.max' => 'La descrizione delle prestazioni non può superare i 150 caratteri.',
             'fields.required' => 'Seleziona almeno una specializzazione per registrare il tuo profilo.',
