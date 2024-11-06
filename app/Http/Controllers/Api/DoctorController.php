02cb50b131b15f8e079fe02e2947b7662ea308e7
pierobramanti <piero.bramanti94@gmail.com>
2024-11-06 16:09:39 +0100

    [UPD] implemented index method in DoctorController to return all doctors in JSON format

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Doctor;
 
 class DoctorController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        $doctors = Doctor::all(); 
+        return response()->json([
+            'success' => true,
+            'results' => $doctors
+        ]);
+    }
 }
