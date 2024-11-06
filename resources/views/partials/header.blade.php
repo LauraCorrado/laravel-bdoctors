461dd91e5345551d6a041219176b94271c15845a
Corfaya <lauracorrado0696@gmail.com>
2024-11-06 16:46:35 +0100

    [UPD] changed dropdown menu name into the latest

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -37,7 +37,7 @@
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                             data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
+                            {{  Auth::user()->doctor && Auth::user()->doctor->user_name ?  Auth::user()->doctor->user_name : Auth::user()->name }}
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
