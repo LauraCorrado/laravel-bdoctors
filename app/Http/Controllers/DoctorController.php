e8e9e38994bea3c208c9d521d9b59f7888350e7d
Corfaya <lauracorrado0696@gmail.com>
2024-11-06 13:23:18 +0100

    [FIX] fix slug and safe routes

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -42,24 +42,24 @@ class DoctorController extends Controller
      */
     public function store(StoreDoctorRequest $request, Doctor $doctor)
     {
-        
+
         $form_data = $request->validated();
 
         $form_data['user_name'] = auth()->user()->name;
         $form_data['user_surname'] = auth()->user()->surname;
 
-        $slug = Doctor::createSlug($form_data['user_name'].' '.$form_data['user_surname']);
+        $slug = Doctor::createSlug($form_data['user_name'] . ' ' . $form_data['user_surname']);
         while (Doctor::where('slug', $slug)->exists()) {
             // Aggiungi un suffisso casuale allo slug se già esiste nel database
             $slug = Doctor::createSlug($form_data['user_name'] . ' ' . $form_data['user_surname'] . '-' . rand(1000, 9999));
         }
         $form_data['slug'] = $slug;
 
-        if($request->hasFile('thumb')){
+        if ($request->hasFile('thumb')) {
             $path = Storage::disk('public')->put('thumb', $form_data['thumb']);
             $form_data['thumb'] = $path;
         }
-        if($request->hasFile('cv')){
+        if ($request->hasFile('cv')) {
             $path = Storage::disk('public')->put('cv', $form_data['cv']);
             $form_data['cv'] = $path;
         }
@@ -68,13 +68,13 @@ class DoctorController extends Controller
         $doctor->user_id = auth()->id();
         $doctor->save();
 
-        if($request->has('fields')) {
+        if ($request->has('fields')) {
             //salvo il valore del campo (l'array di id)
             $fields = $request->fields;
             // attach()->prendo array di fields e creo record nella pivot che rappresenta la relazione m-to-m
             $doctor->fields()->attach($fields);
         }
-        
+
         return redirect()->route('admin.doctors.show', ['doctor' => $doctor->slug]);
     }
 
@@ -119,23 +119,32 @@ class DoctorController extends Controller
      */
     public function update(UpdateDoctorRequest $request, Doctor $doctor)
     {
-        if ($doctor->user_id !== auth()->id()) {
+        if ($doctor->user->id !== auth()->id()) {
             abort(403, 'Azione non autorizzata.');
         }
 
         $form_data = $request->validated();
 
-        $form_data['slug'] = Doctor::createSlug($form_data['user_name'] . $form_data['user_surname']);
+        // Se il nome o il cognome sono cambiati, aggiorna il slug
+        if ($doctor->user_name !== $form_data['user_name'] || $doctor->user_surname !== $form_data['user_surname']) {
+            $slug = Doctor::createSlug($form_data['user_name'] . ' ' . $form_data['user_surname']);
+            while (Doctor::where('slug', $slug)->exists()) {
+                // Aggiungi un suffisso casuale allo slug se già esiste nel database
+                $slug = Doctor::createSlug($form_data['user_name'] . ' ' . $form_data['user_surname'] . '-' . rand(1000, 9999));
+            }
+            $form_data['slug'] = $slug;
+        }
 
-        if($request->hasFile('thumb')){
-            if(!Str::startsWith($doctor->thumb, 'https')){
+        if ($request->hasFile('thumb')) {
+            if (!Str::startsWith($doctor->thumb, 'https')) {
                 Storage::disk('public')->delete($doctor->thumb);
             }
             $path = Storage::disk('public')->put('thumb', $form_data['thumb']);
             $form_data['thumb'] = $path;
         }
-        if($request->hasFile('thumb')){
-            if($doctor->cv){
+
+        if ($request->hasFile('cv')) {
+            if ($doctor->cv) {
                 Storage::disk('public')->delete($doctor->cv);
             }
             $path = Storage::disk('public')->put('cv', $form_data['cv']);
@@ -150,9 +159,7 @@ class DoctorController extends Controller
             $doctor->fields()->sync($fields);
         }
 
-        
         return redirect()->route('admin.doctors.show', ['doctor' => $doctor->slug]);
-                     
     }
 
     /**
