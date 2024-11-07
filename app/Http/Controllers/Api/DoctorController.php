a4948860bd4257ca4f6f1bde4d7a7d4f7fea0cb8
Corfaya <lauracorrado0696@gmail.com>
2024-11-07 12:32:16 +0100

    [UPD]] api controller updated with fields

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Doctor;
+use App\Models\Field;
 
 class DoctorController extends Controller
 {
@@ -17,4 +18,25 @@ class DoctorController extends Controller
             'results' => $doctors
         ]);
     }
+
+    public function getFields() {
+        $fields = Field::all();
+        return response()->json([
+            'success' => true,
+            'results' => $fields
+        ]);
+    }
+
+    // public function searchByFields(Request $request) {
+    //     // inizializziamo una query per Doctor
+    //     $query = Doctor::query();
+
+    //     // --- FILTRO ----
+    //     if($request->filled('field')) {
+    //         // whereHas per filtrare in base a relazione esistente
+    //         $query->whereHas('fields', function ($qu) use ($request) {
+    //             $qu->where('name', 'like')
+    //         })
+    //     }
+    // }
 }
