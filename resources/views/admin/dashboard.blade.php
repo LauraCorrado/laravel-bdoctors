961920e46f6e6b51e13d5e0d73d7e132f747487c
mariannacoppa <densimetra@gmail.com>
2024-11-08 16:56:21 +0100

    [UPD] added dashboard buttons (the edit one is working, others are static)

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,9 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header"><h5 class="m-0">Benvenuto/a, {{$doctor->user_name}}</h5></div>
+                <div class="card-header">
+                    <h5 class="m-0">Benvenuto/a, {{$doctor->user_name}}</h5>
+                </div>
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
@@ -16,7 +18,23 @@
                     </div>
                     @endif
                     <p>{{ __('Hai effettuato con successo il login!') }}</p>
-                    <p class="m-0">{{ __('Questa è la tua dashboard.') }}</p>
+                    <p class="mt-1">{{ __('Questa è la tua dashboard.') }}</p>
+                    <div class="mt-4 show-buttons">
+                        <a href="{{ route('admin.doctors.edit', $doctor->id) }}" class="text-decoration-none save">{{
+                            __('Modifica i tuoi dati') }}</a>
+                    </div>
+                    <div class="mt-4 show-buttons">
+                        <a class="mt-1 text-decoration-none save">{{ __('I tuoi messaggi') }}</a>
+                    </div>
+                    <div class="mt-4 show-buttons">
+                        <a class="mt-1 text-decoration-none save">{{ __('Cosa dicono di te i pazienti') }}</a>
+                    </div>
+                    <div class="mt-4 show-buttons">
+                        <a class="mt-1 text-decoration-none save">{{ __('Promuovi il tuo profilo') }}</a>
+                    </div>
+                    <div class="mt-4 show-buttons">
+                        <a class="mt-1 text-decoration-none save">{{ __('Le tue statistiche') }}</a>
+                    </div>
                 </div>
             </div>
         </div>
