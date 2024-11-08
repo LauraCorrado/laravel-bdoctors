23f20db4263b18beb6c67478c58070f276b26171
mariannacoppa <densimetra@gmail.com>
2024-11-08 16:16:31 +0100

    [UPD] created reviews file

---
+++ config/reviews.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    ["id" => 1, "name" => "Giovanni", "email" => "giovanni@example.com", "content" => "Molto soddisfatto del trattamento ricevuto. Consiglio vivamente questo dottore.", "doctor_id" => 5],
+    ["id" => 2, "name" => "Maria", "email" => "maria@example.com", "content" => "Il medico è stato molto professionale e ha risolto il mio problema in breve tempo.", "doctor_id" => 2],
+    ["id" => 3, "name" => "Luca", "email" => "luca@example.com", "content" => "Dottore competente, ma un po' troppo tempo d'attesa per la visita.", "doctor_id" => 3],
+    ["id" => 4, "name" => "Sofia", "email" => "sofia@example.com", "content" => "Esperienza positiva, sicuramente tornerò per un altro controllo.", "doctor_id" => 7],
+    ["id" => 5, "name" => "Alessandro", "email" => "alessandro@example.com", "content" => "Un medico molto empatico, mi ha messo subito a mio agio. Lo consiglio.", "doctor_id" => 9],
+    ["id" => 6, "name" => "Francesca", "email" => "francesca@example.com", "content" => "Purtroppo non sono rimasta soddisfatta. La diagnosi non è stata corretta.", "doctor_id" => 1],
+    ["id" => 7, "name" => "Paolo", "email" => "paolo@example.com", "content" => "Ottima esperienza, il dottore ha risposto a tutte le mie domande in maniera chiara.", "doctor_id" => 4],
+    ["id" => 8, "name" => "Elena", "email" => "elena@example.com", "content" => "Il dottore è stato molto gentile e competente. Lo raccomando a chiunque.", "doctor_id" => 10],
+    ["id" => 9, "name" => "Giorgio", "email" => "giorgio@example.com", "content" => "Una visita veloce ma accurata. Il trattamento mi ha aiutato molto.", "doctor_id" => 6],
+    ["id" => 10, "name" => "Laura", "email" => "laura@example.com", "content" => "Il medico mi ha dato una spiegazione esauriente riguardo la mia malattia. Molto soddisfatta.", "doctor_id" => 15],
+    ["id" => 11, "name" => "Simone", "email" => "simone@example.com", "content" => "Un po' troppo caro per la visita, ma la qualità del servizio è alta.", "doctor_id" => 13],
+    ["id" => 12, "name" => "Giulia", "email" => "giulia@example.com", "content" => "Il dottore è molto disponibile e professionale. Mi sono trovata benissimo.", "doctor_id" => 14],
+    ["id" => 13, "name" => "Antonio", "email" => "antonio@example.com", "content" => "La visita è stata rapida e il trattamento è stato efficace. Grazie!", "doctor_id" => 8],
+    ["id" => 14, "name" => "Chiara", "email" => "chiara@example.com", "content" => "Un'esperienza eccellente! Molto competente e attento alle esigenze del paziente.", "doctor_id" => 11],
+    ["id" => 15, "name" => "Marco", "email" => "marco@example.com", "content" => "Visita molto soddisfacente. Il medico è stato molto professionale e cortese.", "doctor_id" => 12]
+];
+
+
+?>
