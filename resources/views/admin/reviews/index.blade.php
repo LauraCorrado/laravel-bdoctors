f1e931a9e6939e153b19262f9a3c84199cb172d3
mariannacoppa <densimetra@gmail.com>
2024-11-12 17:48:35 +0100

    [UPD] changed reviews dashboard style

--- resources/views/admin/reviews/index.blade.php
+++ resources/views/admin/reviews/index.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 <div class="container py-5">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-12 text-center mb-5">
-            <h1>{{ $doctor->user_name }}, ecco le tue recensioni</h1>
+            <h1 class="text-center fw-bold mb-5" style="color: #005792">{{ $doctor->user_name }}, ecco le tue recensioni</h1>
         </div>
     </div>
-    <div class="row">
+    <div class="row gy-4">
         <div class="col-12">
             @if ($reviews->isEmpty())
             <p class="text-center my-4 text-danger fs-5">Non ci sono recensioni da visualizzare.</p>
             @else
             @foreach ($reviews as $review)
-            <div class="card mb-4 reviews-box">
+            <div class="card mb-4 messages-box">
                 <div class="card-body d-flex flex-wrap align-items-center justify-content-between">
                     <div class="d-flex flex-column flex-wrap flex-md-row info-user">
                         <h6 class="text-danger id">(#{{$review->id}})</h6>
