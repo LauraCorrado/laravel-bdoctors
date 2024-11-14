f7719fcd77c194efb672661d359a9d1e94d4e1eb
Corfaya <lauracorrado0696@gmail.com>
2024-11-14 14:56:22 +0100

    [UPD] homepage style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,21 @@
     </div>
 </div>
 
-<div class="welcome-text-section text-center py-5">
+<div class="py-5 welcome-section">
     <div class="container">
-        <p class="lead bg-blur-panel">
-            Qui potrai creare e gestire il tuo profilo medico in modo semplice e intuitivo. Se sei un medico, potrai registrarti, aggiornare le tue informazioni, visualizzare messaggi e recensioni ricevute, sponsorizzare il tuo profilo e monitorare le tue statistiche personali. I visitatori potranno cercare medici specialisti, visualizzare i dettagli dei professionisti e inviare richieste di disponibilità, garantendo un’interazione diretta e trasparente tra medico e paziente.
-        </p>
+        <div class="row">
+            <div class="col-12">
+                <div class="text-center homepage-text-box p-5 rounded">
+                    <h2 class="mb-3 title-box">Crea e gestisci il tuo profilo medico in modo semplice e intuitivo!</h2>
+                    <p>Se sei un medico, potrai <a href="{{ route('register') }}" class="link-homepage position-relative text-decoration-none">registrarti <i class="bi bi-box-arrow-up-right small-icon position-absolute"></i></a>, aggiornare le tue informazioni, visualizzare i messaggi e le recensioni ricevute, sponsorizzare il tuo profilo e monitorare le tue statistiche personali.</p>
+                    <p>I visitatori potranno cercare medici specialisti, visualizzare i dettagli dei professionisti e inviare richieste di disponibilità, garantendo un’interazione diretta e trasparente tra medico e paziente.</p>
+                    <div class="pt-3 d-flex justify-content-center align-items-center buttons-home">
+                        <a class="text-decoration-none button-home btn-register" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                        <a class="text-decoration-none button-home btn-login" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
