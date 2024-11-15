2f557488b33cb3334c53b45ea7c8d2d0178f7721
Corfaya <lauracorrado0696@gmail.com>
2024-11-15 17:53:54 +0100

    [UPD] dynamic titles for each page

--- resources/views/admin/stats/index.blade.php
+++ resources/views/admin/stats/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'BD - Statistiche')
 @section('content')
 
 <div class="container py-5">
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'BD - Accesso')
 @section('content')
 <div class="container mt-4">
     <div class="row justify-content-center">
