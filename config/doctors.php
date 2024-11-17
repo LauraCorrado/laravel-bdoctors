3a610fc9e6564f83cf5505edcff59e0f526c471d
Corfaya <lauracorrado0696@gmail.com>
2024-11-17 16:45:47 +0100

    fix: cv paths

--- config/doctors.php
+++ config/doctors.php
@@ -8,7 +8,7 @@ return [
         "city" => "Bergamo",
         "address" => "Via Manzoni, 12",
         "phone_number" => "+39 06 12345678",
-        "cv" => "/curricula/house.pdf",
+        "cv" => "house.pdf",
         "thumb" => "http://127.0.0.1:8000/img/gregory-house.jpg",
         "performance" => "Eseguo una valutazione completa del paziente, raccogliendo un'anamnesi dettagliata e analizzando con attenzione i sintomi."
     ],
@@ -19,7 +19,7 @@ return [
         "city" => "Milano",
         "address" => "Corso Magenta, 45",
         "phone_number" => "+39 02 7654321",
-        "cv" => "/curricula/wilson.pdf",
+        "cv" => "wilson.pdf",
         "thumb" => "http://127.0.0.1:8000/img/james-wilson.jpg",
         "performance" => "Mi occupo di fornire un'assistenza completa al paziente, concentrandomi sia sulle cure fisiche che sul supporto emotivo."
     ],
@@ -30,7 +30,7 @@ return [
         "city" => "Napoli",
         "address" => "Piazza Garibaldi, 78",
         "phone_number" => "+39 081 9876543",
-        "cv" => "/curricula/foreman.pdf",
+        "cv" => "foreman.pdf",
         "thumb" => "http://127.0.0.1:8000/img/eric-foreman.jpg",
         "performance" => "Effettuo visite di controllo periodiche per valutare il benessere complessivo e lo stato di salute del paziente."
     ],
@@ -41,7 +41,7 @@ return [
         "city" => "Torino",
         "address" => "Via Po, 100",
         "phone_number" => "+39 011 4567890",
-        "cv" => "/curricula/cuddy.pdf",
+        "cv" => "cuddy.pdf",
         "thumb" => "http://127.0.0.1:8000/img/lisa-cuddy.jpg",
         "performance" => "Conduco un esame fisico accurato e consiglio, se necessario, ulteriori accertamenti diagnostici."
     ],
@@ -52,7 +52,7 @@ return [
         "city" => "Firenze",
         "address" => "Viale Michelangelo, 15",
         "phone_number" => "+39 055 1239876",
-        "cv" => "/curricula/cameron.pdf",
+        "cv" => "cameron.pdf",
         "thumb" => "http://127.0.0.1:8000/img/allison-cameron.jpg",
         "performance" => "Effettuo diagnosi precise e creo piani di trattamento personalizzati per ogni paziente."
     ],
@@ -63,7 +63,7 @@ return [
         "city" => "Venezia",
         "address" => "Riva degli Schiavoni, 32",
         "phone_number" => "+39 041 3456789",
-        "cv" => "/curricula/park.pdf",
+        "cv" => "park.pdf",
         "thumb" => "http://127.0.0.1:8000/img/chi-park.jpg",
         "performance" => "Prescrivo esami di laboratorio e mi occupo di interpretare i risultati per garantire una diagnosi accurata."
     ],
@@ -74,7 +74,7 @@ return [
         "city" => "Venezia",
         "address" => "Via XX Settembre, 11",
         "phone_number" => "+39 010 8765432",
-        "cv" => "/curricula/taub.pdf",
+        "cv" => "taub.pdf",
         "thumb" => "http://127.0.0.1:8000/img/chris-taub.jpg",
         "performance" => "Somministro trattamenti farmacologici e monitoro gli effetti collaterali per adattare la terapia, se necessario."
     ],
@@ -85,7 +85,7 @@ return [
         "city" => "Bologna",
         "address" => "Via Rizzoli, 45",
         "phone_number" => "+39 051 6543210",
-        "cv" => "/curricula/hudley.pdf",
+        "cv" => "hudley.pdf",
         "thumb" => "http://127.0.0.1:8000/img/remy-hadley.jpg",
         "performance" => "Pianifico e seguo terapie a lungo termine per supportare i pazienti nella gestione delle loro condizioni."
     ],
@@ -96,7 +96,7 @@ return [
         "city" => "Bologna",
         "address" => "Corso Vittorio Emanuele, 9",
         "phone_number" => "+39 091 1234567",
-        "cv" => "/curricula/chase.pdf",
+        "cv" => "chase.pdf",
         "thumb" => "http://127.0.0.1:8000/img/robert-chase.jpg",
         "performance" => "Eseguo test di screening e valuto i fattori di rischio, personalizzando i piani di prevenzione."
     ],
@@ -107,7 +107,7 @@ return [
         "city" => "Roma",
         "address" => "Via Catanzaro, 150",
         "phone_number" => "+39 070 2345678",
-        "cv" => "/curricula/martini.pdf",
+        "cv" => "martini.pdf",
         "thumb" => "http://127.0.0.1:8000/img/lele-martini.jpg",
         "performance" => "Mi occupo di interpretare i risultati di test diagnostici e prescrivere il trattamento migliore per ogni paziente."
     ],
